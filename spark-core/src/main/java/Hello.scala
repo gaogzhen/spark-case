fcc42a71706e3abc8c0c98b0e7a139c9f9dff5a4
gaogzhen <gaogzhen@foxmail.com>
2023-10-08 08:04:57 +0800

    init

---
+++ spark-core/src/main/java/Hello.scala
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author gaogzhen
+ * @since 2023/10/8 07:54
+ */
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("hello spark")
+  }
+}
---
+++ spark-core/src/main/scala/com/gaogzhen/bigdata/spark/core/Hello.scala
@@ -0,0 +1,12 @@
+package com.gaogzhen.bigdata.spark.core
+
+/**
+ *
+ * @author gaogzhen
+ * @since 2023/10/8 07:53
+ */
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("hello spark")
+  }
+}
